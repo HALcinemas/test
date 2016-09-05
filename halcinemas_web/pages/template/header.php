f95ec83db20d7a992ddd5708e0b4ce11d62e5387
shizuchaos <mhisss1113@gmail.com>
2016-09-05 12:05:21 +0900

    test

---
+++ halcinemas_web/pages/template/header.php
@@ -0,0 +1,17 @@
+<!-- header template -->
+<div class="header">
+	<img src="#" alt="hal cinema logo">
+	<div class="header__primary">
+		<a href="#">ログイン</a>
+		<a href="#">新規登録</a>
+	</div>
+	<div class="header__nav">
+		<ul class="header__nav--ul">
+			<li class="header__nav--li">HOME</li>
+			<li class="header__nav--li">THEATER</li>
+			<li class="header__nav--li">MOVIE</li>
+			<li class="header__nav--li">SERVICE</li>
+			<li class="header__nav--li">CONTACT</li>
+		</ul>
+	</div>
+</div>
