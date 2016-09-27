05d02b686adc31b001386ebf4adbee0cbee225b6
shizuchaos <mhisss1113@gmail.com>
2016-09-27 10:01:16 +0900

    footer

--- halcinemas_web/pages/template/footer.php
+++ halcinemas_web/pages/template/footer.php
@@ -1,21 +1,21 @@
 <!-- footer template -->
 <div class="footer">
-	<img src="#" alt="footer hal cinema logo">
+	<img src="#" alt="footer hal cinema logo" class="footer-logo">
 	<div class="footer__nav">
 		<ul class="footer__nav--ul">
-			<li class="footer__nav--li">HOME</li>
-			<li class="footer__nav--li">THEATER</li>
-			<li class="footer__nav--li">MOVIE</li>
-			<li class="footer__nav--li">SERVICE</li>
-			<li class="footer__nav--li">CONTACT</li>
+			<li class="footer__nav--li"><a href="#" class="footer__nav--li-a">HOME</a></li>
+			<li class="footer__nav--li"><a href="#" class="footer__nav--li-a">THEATER</a></li>
+			<li class="footer__nav--li"><a href="#" class="footer__nav--li-a">MOVIE</a></li>
+			<li class="footer__nav--li"><a href="#" class="footer__nav--li-a">SERVICE</a></li>
+			<li class="footer__nav--li"><a href="#" class="footer__nav--li-a">CONTACT</a></li>
 		</ul>
 	</div>
 	<div class="footer__menu">
 		<ul class="footer__menu--ul">
-			<li class="footer__menu--li">個人情報の取り扱いについて</li>
-			<li class="footer__menu--li">利用規約</li>
-			<li class="footer__menu--li">会社概要</li>
-			<li class="footer__menu--li">お問い合わせ</li>
+			<li class="footer__menu--li"><a href="#" class="footer__menu--li-a">個人情報の取り扱いについて</a></li>
+			<li class="footer__menu--li"><a href="#" class="footer__menu--li-a">利用規約</a></li>
+			<li class="footer__menu--li"><a href="#" class="footer__menu--li-a">会社概要</a></li>
+			<li class="footer__menu--li"><a href="#" class="footer__menu--li-a">お問い合わせ</a></li>
 		</ul>
 	</div>
 	<div class="footer__copy">
