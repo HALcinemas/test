5629ef6641a7dcaa6b85acaed35bd31d5b3ea168
shizuchaos <mhisss1113@gmail.com>
2016-11-01 13:00:47 +0900

    ファイル名変更とアップロード

---
+++ halcinemas_web/pages/一般/THEATER/theater_list.php
@@ -0,0 +1,21 @@
+<<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>THEATER LIST</title>
+	<link rel="stylesheet" type="text/css" href="../css/reset.css">
+	<link rel="stylesheet" type="text/css" href="../css/style.min.css">
+</head>
+<body>
+	<header>
+		<?php 
+			require('template/header.php')
+		?>
+	</header>
+	<footer>
+		<?php 
+			require('template/footer.php')
+		?>
+	</footer>
+</body>
+</html>
