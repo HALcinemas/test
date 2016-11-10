02d3c4ed0403ac7122d3d8b73b0764805e84e1d7
shizuchaos <mhisss1113@gmail.com>
2016-11-10 12:36:40 +0900

    theater個別ページもろもろ

---
+++ halcinemas_web/pages/一般/theater_page.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>THEATER LIST</title>
+	<link rel="stylesheet" type="text/css" href="../../css/reset.css">
+	<link rel="stylesheet" type="text/css" href="../../css/style.min.css">
+	<script src="../../js/script.js"></script>
+</head>
+<body>
+	<header>
+		<?php 
+			require('template/header.php')
+		?>
+	</header>
+    <div class="theater-content">
+        <h2 class="theater-content_title">HAL CINEMAS 店舗名</h2>
+        <ul class="theater-content__ul">
+            <li class="theater-content__li"><span class="theater-content__li-text">*月*日（曜）</span></li>
+            <li class="theater-content__li"><span class="theater-content__li-text">*月*日（曜）</span></li>
+            <li class="theater-content__li"><span class="theater-content__li-text">*月*日（曜）</span></li>
+            <li class="theater-content__li"><span class="theater-content__li-text">*月*日（曜）</span></li>
+            <li class="theater-content__li"><span class="theater-content__li-text">*月*日（曜）</span></li>
+        </ul>
+        <h3 class="theater-content_date">*月*日（曜）</h3>
+        <div class="theater">
+            <h3 class="theater-title">映画タイトル</h3>
+            <img src="#">
+            <div class="theater-screen">
+                <h4 class="theater-screen_num">SCREEN*</h4>
+                <ul class="theater-screen__ul">
+                    <li class="theater-screen__li">
+                        <a href="#" class="theater-screen__link">
+                            <span class="theater-screen__time">*:**-*:**</span>
+                            <span class="theater-screen__states">○空席あり</span>
+                        </a>
+                    </li>
+                    <li class="theater-screen__li">
+                        <a href="#" class="theater-screen__link">
+                            <span class="theater-screen__time">*:**-*:**</span>
+                            <span class="theater-screen__states">○空席あり</span>
+                        </a>
+                    </li>
+                    <li class="theater-screen__li">
+                        <a href="#" class="theater-screen__link">
+                            <span class="theater-screen__time">*:**-*:**</span>
+                            <span class="theater-screen__states">○空席あり</span>
+                        </a>
+                    </li>
+                    <li class="theater-screen__li">
+                        <a href="#" class="theater-screen__link">
+                            <span class="theater-screen__time">*:**-*:**</span>
+                            <span class="theater-screen__states">○空席あり</span>
+                        </a>
+                    </li>
+                    <li class="theater-screen__li">
+                        <a href="#" class="theater-screen__link">
+                            <span class="theater-screen__time">*:**-*:**</span>
+                            <span class="theater-screen__states">○空席あり</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <footer>
+		<?php 
+			require('template/footer.php')
+		?>
+	</footer>
+</body>
+</html>
