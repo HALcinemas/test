167dadeaacfeab11f5dd62b9a7b40b4f6a568cad
shizuchaos <mhisss1113@gmail.com>
2016-11-22 12:40:40 +0900

    追加

--- halcinemas_web/pages/一般/cinema_list.php
+++ halcinemas_web/pages/一般/cinema_list.php
@@ -18,12 +18,32 @@
         <a href="#" class="cinema-list_bread">上映中作品一覧</a>
         <h2 class="cinema-list_h2">上映中作品</h2>
         <span class="cinema-list_pull">作品リスト：</span>
-        <select>
-            <option>作品01</option>
-            <option>作品02</option>
-            <option>作品03</option>
+        <select class="cinema-list_select"> 
+            <option class="cinema-list_option">作品01</option>
+            <option class="cinema-list_option">作品02</option>
+            <option class="cinema-list_option">作品03</option>
         </select>
+        <ul class="cinema-list_ul">
+            <!-- 繰り返し -->
+            <li class="cinema-list_li">
+                <img src="#" class="cinema-list_li-img">
+                    <h3 class="cinema-list_li-title">title title title</h3>
+                    <!-- 監督 -->
+                    <div class="line">
+                        <span class="b-box">監督</span>
+                        <span class="directer">name</span>
+                    </div>
+                    <!-- 出演 -->
+                    <div class="line">
+                        <span class="b-box">出演</span>
+                        <span class="performer">name</span>
+                    </div>
+                    <a href="#" class="cinema-list_li-det">詳しく見る</a>
+                </div>
+            </li>
+        </ul>
     </div>
+    
     <footer>
 		<?php 
 			require('template/footer.php')
