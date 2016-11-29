5a718c461d4e97c8cc4d52b73c42970b1c9970e3
浦野 <urano@urano-no-MacBook-Air.local>
2016-11-29 12:38:17 +0900

    サービス案内、利用規約（網）

---
+++ halcinemas_web/pages/一般/servis.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="../../css/annai.css">
+  <title>Document</title>
+</head>
+<body>
+  <!-- header template -->
+  <header>
+  		<?php
+  			require('template/header.php')
+  		?>
+  	</header>
+  <div class="container">
+    <div class="service">
+      <h2>サービス案内</h2>
+      <h3>インターネットチケット販売</h3>
+        <p>インターネットでの購入・支払いまでができる便利なチケット販売システムです。
+        ご覧になりたい映画のチケットをパソコンや、外出先でもスマートフォンから事前に購入することができるため、
+        混み合う人気作品でも窓口に並ぶ必要がありません。</p>
+      <div class="buttonbox">
+        <button>詳しくはこちら</button>
+      </div>
+    </div>
+    <div class="service">
+      <h3>シネマイレージ</h3>
+        <p>5回観たら１回無料のサービスのほか、鑑賞時間の長さでマイルが貯まり、ポップコーンやドリンクなどと交換することができます。
+        シネマイレージ会員の方は、インターネットチケット購入時にログインID(会員番号)
+        とパスワードを入力することで個人情報の入力作業を省くことができます。</p>
+      <div class="buttonbox">
+        <button>詳しくはこちら</button>
+      </div>
+    </div>
+    <div class="service">
+      <h3>ギフトカード</h3>
+        <p>ギフトカードは、全国のTOHOシネマズで買える・使える、プリペイドカードです。
+        大切なプレゼントシーンのために、贈答用の特別パッケージ(別売り)もご用意しております。</p>
+      <div class="buttonbox">
+        <button>詳しくはこちら</button>
+      </div>
+    </div>
+  </div>
+    <footer>
+    		<?php
+    			require('template/footer.php')
+    		?>
+    	</footer>
+</body>
+</html>
