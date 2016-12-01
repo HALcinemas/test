275f505cb4a687084c8ea7879f519b4900a47c8c
shizuchaos <mhisss1113@gmail.com>
2016-12-01 12:56:20 +0900

    1日分

--- halcinemas_web/pages/一般/cinema-soon_list.php
+++ halcinemas_web/pages/一般/cinema-soon_list.php
@@ -15,17 +15,31 @@
 	</header>
     <div class="cinema-list">
         <a href="#" class="cinema-list_bread">HOME</a>&gt;
-        <a href="#" class="cinema-list_bread">上映中作品一覧</a>
-        <h2 class="cinema-list_h2">上映中作品</h2>
+        <a href="#" class="cinema-list_bread">公開予定作品一覧</a>
+        <h2 class="cinema-list_h2">公開予定作品</h2>
         <span class="cinema-list_pull">作品リスト：</span>
         <select class="cinema-list_select"> 
             <option class="cinema-list_option">作品01</option>
             <option class="cinema-list_option">作品02</option>
             <option class="cinema-list_option">作品03</option>
         </select>
+        <ul class="theater-content__ul">
+            <li class="theater-content__li"><span class="theater-content__li-text">*月*日（曜）</span></li>
+            <li class="theater-content__li"><span class="theater-content__li-text">*月*日（曜）</span></li>
+            <li class="theater-content__li"><span class="theater-content__li-text">*月*日（曜）</span></li>
+            <li class="theater-content__li"><span class="theater-content__li-text">*月*日（曜）</span></li>
+            <li class="theater-content__li"><span class="theater-content__li-text">*月*日（曜）</span></li>
+        </ul>
+        <ul class="cinema-soon_ul">
+            <li class="cinema-soon_li">7月以降</li>
+            <li class="cinema-soon_li">8月以降</li>
+            <li class="cinema-soon_li">9月以降</li>
+            <li class="cinema-soon_li">10月以降</li>
+        </ul>
+        <h4 class="cinema-soon_h4">＊月＊日公開予定</h4>
         <ul class="cinema-list_ul">
             <!-- 繰り返し -->
-            <li class="cinema-list_li">
+            <li class="cinema-list_li cinema-soon_cinema-li">
                 <img src="#" class="cinema-list_li-img">
                 <h3 class="cinema-list_li-title">title title title</h3>
                 <!-- 監督 -->
