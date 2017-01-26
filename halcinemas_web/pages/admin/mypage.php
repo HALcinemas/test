ab00253176e75ced6d1180bde484a8ed6e2b8a7f
shizuchaos <mhisss1113@gmail.com>
2017-01-26 16:06:40 +0900

    マイページやら

---
+++ halcinemas_web/pages/admin/mypage.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="../../css/reset.css">
+	<link rel="stylesheet" type="text/css" href="../../css/style.min.css">
+	<title>CONTACT</title>
+</head>
+<body>
+	<header>
+		<?php 
+			require('../general/template/header.php');
+		?>
+	</header>
+    <div class="mypage">
+        <h2 class="mypage-name">**さんのマイページ</h2>
+        <span class="mypage-point">＊ポイント保有</span>  
+    </div>
+    <?php 
+        require('../general/template/footer.php');
+    ?>	
+</body>
+</html>
