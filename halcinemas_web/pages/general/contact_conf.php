16bfbf553976046a76bfb4bcf0f4875dc181a76b
shizuchaos <mhisss1113@gmail.com>
2017-01-26 12:33:37 +0900

    お問い合わせ完了ページ

---
+++ halcinemas_web/pages/general/contact_conf.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="../../css/reset.css">
+	<link rel="stylesheet" type="text/css" href="../../css/style.min.css">
+	<title>CONTACT</title>
+</head>
+<body>
+	<header>
+		<?php 
+			require('./template/header.php');
+		?>
+	</header>
+    <div class="con-comf">
+        <p class="con-comf_text">お問い合わせ送信完了しました。</p>
+        <a href="index.php" class="con-comf-link"><span class="con-comf-link_text">TOPへもどる</span></a>
+        
+    </div>
+    <?php 
+        require('./template/footer.php');
+    ?>	
+</body>
+</html>
