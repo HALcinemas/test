93d4dce8b17f2040ef3f70d12883b6bc3f98f8fa
shizuchaos <mhisss1113@gmail.com>
2017-01-26 12:10:37 +0900

    問い合わせページ

--- halcinemas_web/pages/general/contact.php
+++ halcinemas_web/pages/general/contact.php
@@ -20,29 +20,36 @@
             ※ お問い合わせを確認させていただく時間は、土日祝日を除く、平日9時30分から18時30分となっております。（祝日にはゴールデンウィーク並びに年末年始を含みます）
         </p>
         <form action="contact_comf.php" method="POST" class="contact-form">    
-            <table>
-                <tr>
-                    <th><label class="contact-label">お問い合わせ種類</label></th>
-                    <td><input type="text" name="" value="" placeholder="〇〇について"></td>
+            <table class="contact-table">
+                <tr class="contact-tr">
+                    <th class="contact-th"><label class="contact-label">お問い合わせ種類</label></th>
+                    <td class="contact-td"><input type="text" class="contact-input" name="" value="" placeholder="〇〇について"></td>
                 </tr>
-                <tr>
-                    <th><label class="contact-label">氏名</label></th>
-                    <td><input type="text" name="" value="" placeholder="姓"><input type="text" name="" value="" placeholder="名"></td>
+                <tr class="contact-tr">
+                    <th class="contact-th"><label class="contact-label">氏名</label></th>
+                    <td class="contact-td"><input type="text" class="contact-input" name="" value="" placeholder="姓"><input type="text" class="contact-input" name="" value="" placeholder="名"></td>
                 </tr>
-                <tr>
-                    <th><label class="contact-label">電話番号</label></th>
-                    <td><input type="text" name="" value="" placeholder="ハイフンなし半角数字"></td>
+                <tr class="contact-tr">
+                    <th class="contact-th"><label class="contact-label">電話番号</label></th>
+                    <td class="contact-td"><input type="text" class="contact-input" name="" value="" placeholder="ハイフンなし半角数字"></td>
                 </tr>
-                <tr>
-                    <th><label class="contact-label">メールアドレス</label></th>
-                    <td><input type="text" name="" value="" placeholder="exam@mail.com"></td>
+                <tr class="contact-tr">
+                    <th class="contact-th"><label class="contact-label">メールアドレス</label></th>
+                    <td class="contact-td"><input type="text" class="contact-input" name="" value="" placeholder="exam@mail.com"></td>
                 </tr>
-                <tr>
-                    <th><label class="contact-label">内容</label></th>
-                    <td><textarea rows="" cols="" placeholder="ご意見・ご要望"></textarea></td>
+                <tr class="contact-tr">
+                    <th class="contact-th">
+                        <label for="hoge" class="contact-label">内容</label>
+                    </th>
+                    <td class="contact-td"><textarea id="hoge" class="contact-tarea" rows="10" cols="70" placeholder="ご意見・ご要望"></textarea></td>
                 </tr>
-                <td><button type="submit" class="contact-button">確認</button></td>
             </table>
+             <p class="contact-text_conf">利用規約および個人情報の取り扱いについて（プライバシーポリシー）をご確認いただき、同意いただける場合は「同意して送信」を押してください。</p>
+                <td class="contact-td">
+                    <div class="center">
+                        <button type="submit" class="contact-button">同意して送信</button>
+                    </div>
+                </td>
         </form>
     </div>
     <?php 
